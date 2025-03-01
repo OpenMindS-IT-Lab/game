1bc655229d39138ecf17cb833069c0e448ffa2c3
Sam <joncolab@duck.com>
2025-03-01 13:32:44 +0200

    Update `deploy-succeeded.ts` Netlify Function

--- server/deploy-succeeded.ts
+++ server/deploy-succeeded.ts
@@ -1,10 +1,8 @@
-import { Handler } from '@netlify/functions'
-
-export const handler: Handler = () => {
+export default async () => {
   const setWebhookUrl = process.env.SET_WEBHOOK_URL
   console.log('⏳ Setting up Telegram Bot webhook')
   if (!!setWebhookUrl)
-    fetch(setWebhookUrl).then(
+    await fetch(setWebhookUrl).then(
       () => console.log('✅ Telegram Bot webhook set'),
       () => console.log('❌ Error setting Telegram Bot webhook')
     )
